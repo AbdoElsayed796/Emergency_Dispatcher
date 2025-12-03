30bbc6b106a3809dd9303bb417fbcbc3d8838bde
AbdoElsayed796 <abdelrahmanelsayedgo2@gmail.com>
2025-12-03 04:34:59 +0200

    Add initial frontend components and API services for user authentication and management

---
+++ frontend/src/api/services/index.js
@@ -0,0 +1,5 @@
+export { default as userService } from './userService';
+export { default as stationService } from './stationService';
+export { default as vehicleService } from './vehicleService';
+export { default as incidentService } from './incidentService';
+export { default as assignmentService } from './assignmentService';
